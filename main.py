55b6ec4586de3f5352b393b02159a9bb4e8612b0
ShahabAthar25 <shahabathar25@gmail.com>
2021-10-01 10:56:48 +0500

    Add Fps

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ mp_hands = mp.solutions.hands
 hands = mp_hands.Hands()
 mp_draw = mp.solutions.drawing_utils
 
+previous_time = 0
+current_time = 0
+
 while True:
     success, img = video.read()
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -16,6 +19,12 @@ while True:
     if results.multi_hand_landmarks:
         for hand_landmark in results.multi_hand_landmarks:
             mp_draw.draw_landmarks(img, hand_landmark, mp_hands.HAND_CONNECTIONS)
+
+    current_time = time.time()
+    fps = 1 / (current_time - previous_time)
+    previous_time = current_time
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
 
 
     cv2.imshow("Hand Detector", img)
