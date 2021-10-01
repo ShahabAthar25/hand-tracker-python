9ce53cd356b802510475bbcf9e0bceb37e6f59d6
ShahabAthar25 <shahabathar25@gmail.com>
2021-10-01 10:38:38 +0500

    Add hand landmarks

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ video = cv2.VideoCapture(0)
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils
 
 while True:
     success, img = video.read()
@@ -12,6 +13,10 @@ while True:
     results = hands.process(img_rgb)
 
     print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+        for hand_landmark in results.multi_hand_landmarks:
+            mp_draw.draw_landmarks(img, hand_landmark)
 
 
     cv2.imshow("Hand Detector", img)
