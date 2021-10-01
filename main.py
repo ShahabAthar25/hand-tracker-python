ad2f27145871b667aa5b02a945fb977ffdf38cc3
ShahabAthar25 <shahabathar25@gmail.com>
2021-10-01 10:35:40 +0500

    Add Hand Detection

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import cv2
+import mediapipe as mp
 
 video = cv2.VideoCapture(0)
 
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+
 while True:
     success, img = video.read()
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(img_rgb)
+
+    print(results.multi_hand_landmarks)
+
 
     cv2.imshow("Hand Detector", img)
     
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
+    cv2.waitKey(1)
